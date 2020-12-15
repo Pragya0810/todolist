84d19e14cae2e957a5399eddc3993a880df56d76
Pragya Chhajer <pragyachhajer48@gmail.com>
2020-12-15 23:45:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-pragya:test123@cluster0.ejyuc.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-pragya:*******@cluster0.ejyuc.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = new mongoose.Schema({
   name:String,
